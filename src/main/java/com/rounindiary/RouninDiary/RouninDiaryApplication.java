33f81f54b6c1529313a9f983dafb7df273769612
higanofront <higanofront@yahoo.co.jp>
2021-09-14 20:27:09 +0900

    initial commit

---
+++ src/main/java/com/rounindiary/RouninDiary/RouninDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.rounindiary.RouninDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouninDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouninDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rounindiary/RouninDiary/RouninDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rounindiary.RouninDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouninDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
